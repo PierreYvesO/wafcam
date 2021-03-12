67de378ed2ec51ba18ac37c25a9ccf9ffd9b9900
abrasset-esiee <alexis.brasset@edu.esiee.fr>
2021-03-12 16:14:12 +0100

    feat: first commit web interface

---
+++ web/src/pages/Cameras.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Cameras = () => {
+  return (
+    <>
+      <Navigation />
+      <div className="Content">
+        <h1>Caméras</h1>
+        <img src="./img/cam1.png" alt="caméra doguito" />
+      </div>
+    </>
+  );
+}
+
+export default Cameras;
---
+++ web/src/pages/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const NotFound = () => {
+  return (
+    <>
+      <Navigation />
+      <div className="Content">
+      <h1>Page inconnue</h1>
+      </div>
+    </>
+  );
+}
+    
+export default NotFound;
+    
