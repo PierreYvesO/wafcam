0ccea00f963a7a9678f21259198063459ee3a2fa
PierreYvesO <oupindrin.pierrey@outlook.com>
2021-03-19 10:23:34 +0100

    more tests

--- python_back/test/test.py
+++ python_back/test/test.py
@@ -9,7 +9,15 @@ user_config = {
 }
 if __name__ == '__main__':
     db = Database(user_config)
-    cam = Camera("http://192.168.1.71:8080/stream.mjpeg", 1, db, size=(1280, 720), display=True)
-    cam.displayCamera()
-    cam.releaseCamera()
+    # cam = Camera("http://192.168.1.71:8080/stream.mjpeg", 1, db, size=(1280, 720), display=True)
+    # cam = Camera("rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream", 1, db, size=(1280, 720), display=True)
+    # cam = Camera("http://wmccpinetop.axiscam.net/mjpg/video.mjpg", 1, db, size=(1280, 720), display=True)
+    # cam = Camera("http://158.58.130.148:80/mjpg/video.mjpg", 1, db, size=(1280, 720), display=True)
+    room_ids = db.getRooms()
+    cams = list()
+    for room_id in room_ids:
+        print(room_id)
+        cams.append(Camera(0, room_id[0], db, size=(1280, 720), display=True))
+    for cam in cams:
+        cam.releaseCamera()
     db.closeConnection()
