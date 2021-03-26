46d0f0806d9b1d831368ffe3b63310691045910f
PierreYvesO <oupindrin.pierrey@outlook.com>
2021-03-26 12:47:11 +0100

    implementation of threaded data sending to database

--- python_back/src/camera.py
+++ python_back/src/camera.py
@@ -1,5 +1,7 @@
 import asyncio
+import threading
 from datetime import datetime
+import time
 
 import cv2
 from python_back.src.detection import Detection
@@ -8,20 +10,43 @@ from cv2.cv2 import VideoCapture
 from queue import Queue
 from python_back.src.database_utils import read_env
 from multiprocessing import Queue as mQueue
+from _queue import Empty
+
+
+def threaded(fn):
+    """
+    Allow to launch function with threads using decorators
+    @param fn: function to be threaded
+    @return: wrapper for thread
+    """
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        thread.daemon = True
+        thread.start()
+        return thread
+
+    return wrapper
 
 
 class Camera:
     def __init__(self, queue, ip, cam_id, size=tuple(), display=False):
         self.cam_queue: mQueue = queue
-        self.ip = ip
+        # self.ip = ip
+        self.ip = 0
         self.display = display
         self.id = cam_id
         self.size = size
         self.cap: VideoCapture
         self.detection: Detection
         self.db = Database(read_env())
+        self.forbidden_access_queue = Queue()
+        self.detection_queue = Queue()
+        self.detection_result = Queue()
         self.startCamera()
-        asyncio.run(self.save_infos())
+
+        # threaded event listeners
+        self.save_infos()
+        self.send_forbidden_access_infos()
 
     def stop_all(self):
         self.releaseCamera()
@@ -37,10 +62,9 @@ class Camera:
             self.cap.set(3, self.size[0])
             self.cap.set(4, self.size[1])
             self.cap.set(10, 150)
-        self.detection_queue = Queue()
-        self.detection_result = Queue()
-        self.detection = Detection(self.detection_queue, self, self.detection_result, self.db.getEntities(),
-                                   self.db.getForbiddenAreas(), self.display)
+
+        self.detection = Detection(self, self.detection_queue, self.detection_result, self.forbidden_access_queue,
+                                   self.db.getEntities(), self.db.getForbiddenAreas(), self.display)
         self.detection.start()
 
     def displayCamera(self):
@@ -70,8 +94,8 @@ class Camera:
     def getID(self):
         return self.id
 
-    async def save_infos(self):
-        from _queue import Empty
+    @threaded
+    def save_infos(self):
         res = {}
         while self.cam_queue.empty():
             try:
@@ -81,7 +105,23 @@ class Camera:
                 time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 for animal in res:
                     self.db.addLog(animal, res[animal], self.id, time)
-                print("result = " + str(res))
+                print("result_log_detected = " + str(res))
                 res = {}
-            await asyncio.sleep(1)
         self.stop_all()
+
+    @threaded
+    def send_forbidden_access_infos(self):
+
+        res = {}
+        while self.cam_queue.empty():
+            try:
+                res = self.forbidden_access_queue.get(timeout=2)
+            except Empty:
+                # on garde la meme date pour tous les envois
+                date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                for animal in res:
+                    # TODO: Add insert for forbidden accesses db
+                    pass
+                print("result_log_in_area = " + str(res))
+                time.sleep(10)
+                res = {}
--- python_back/test/test.py
+++ python_back/test/test.py
@@ -7,18 +7,19 @@ import time
 
 if __name__ == '__main__':
     db = Database(read_env())
-    # cam = Camera("http://192.168.1.71:8080/stream.mjpeg", 1, size=(1280, 720), display=True)
-    # cam = Camera("rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream", 1, size=(1280, 720), display=True)
-    # cam = Camera("http://wmccpinetop.axiscam.net/mjpg/video.mjpg", 1, size=(1280, 720), display=True)
-    # cam = Camera("http://158.58.130.148:80/mjpg/video.mjpg", 1, size=(1280, 720), display=True)
+    # # cam = Camera("http://192.168.1.71:8080/stream.mjpeg", 1, size=(1280, 720), display=True)
+    # # cam = Camera("rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream", 1, size=(1280, 720), display=True)
+    # # cam = Camera("http://wmccpinetop.axiscam.net/mjpg/video.mjpg", 1, size=(1280, 720), display=True)
+    # # cam = Camera("http://158.58.130.148:80/mjpg/video.mjpg", 1, size=(1280, 720), display=True)
     room_ids = db.getRooms()
-    room_ids.append(("rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream", 1))
+    # room_ids.append(("rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream", 1))
     db.closeConnection()
     cams = list()
     queues = list()
     for room_id in room_ids:
         queue = Queue()
         camera_ip = db.getCameraWithID(room_id[2])
+        # camera_ip = 0
         p = Process(target=Camera, args=(queue, camera_ip, room_id[0], (1280, 720), True))
         p.start()
         queues.append(queue)
