351b4ad6109e96d0fd8a27d495e3423b4a8afc95
PierreYvesO <oupindrin.pierrey@outlook.com>
2021-05-21 18:21:14 +0200

    remove call to functions send_update_ws in init

--- python_back/launchWAFCAM.py
+++ python_back/launchWAFCAM.py
@@ -1,14 +1,12 @@
 from simple_websocket_server import WebSocketServer
 
 from python_back.src import database_utils
-from python_back.src.wafcam_websocket import WafcamSocket, launch, send_update_ws
-
+from python_back.src.wafcam_websocket import WafcamSocket, launch
 
 def init_project():
     db_config = database_utils.read_env()
     server = WebSocketServer('localhost', 8000, WafcamSocket, select_interval=None)
     launch(db_config, server)
-    send_update_ws()
     server.serve_forever()
 
 
